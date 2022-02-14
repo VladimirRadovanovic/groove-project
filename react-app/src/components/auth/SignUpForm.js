6dafa1e56d7830ad3b70c225e5367cf9372613bb
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-13 22:26:25 -0500

    form styling done

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect, Link } from 'react-router-dom';
 import { signUp, login } from '../../store/session';
@@ -11,8 +11,32 @@ const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+  const [validClass, setValidClass] = useState('')
+  const [validRepeatPassword, setValidRepeatPassword] = useState('')
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (password.length < 6 && password.length > 0) {
+      setValidClass('input-invalid')
+    } else {
+      setValidClass('')
+    }
+
+    if (repeatPassword.length < 6 && repeatPassword.length > 0 || (repeatPassword !== password && repeatPassword.length > 0)) {
+      console.log(password, repeatPassword)
+      setValidRepeatPassword('repeat-input-invalid')
+    } else {
+      setValidRepeatPassword('')
+    }
+
+    if (email.includes('@') && password.length >= 6 && password === repeatPassword) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [email, password, repeatPassword])
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -90,6 +114,7 @@ const SignUpForm = () => {
           <div className='form-group'>
             <label htmlFor='password-signup'>Password</label>
             <input
+              className={validClass}
               placeholder='*********'
               id='password-signup'
               type='password'
@@ -101,6 +126,7 @@ const SignUpForm = () => {
           <div className='form-group'>
             <label htmlFor='repeat_password'>Repeat Password</label>
             <input
+              className={validRepeatPassword}
               placeholder='*********'
               id='repeat_password'
               type='password'
@@ -111,7 +137,7 @@ const SignUpForm = () => {
             ></input>
           </div>
           <div className='form-button-container margin-bottom-small signup-button'>
-            <button type='submit'>Sign Up</button>
+            <button disabled={buttonDisabled} type='submit'>Register</button>
           </div>
           <div className='form-button-container margin-bottom-small demo-button'>
             <button type='button' onClick={handleDemo}>Demo login</button>
