f030f908f1057b6b328640fcd346fdb9bf98a14e
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-16 16:09:17 -0500

    section 1 splash done

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,4 +1,4 @@
-
+import { NavLink } from 'react-router-dom';
 
 import './SplashPage.css'
 import GetAllListings from '../Listings/GetListings/GetAllListings';
@@ -8,7 +8,12 @@ function SplashPage({ user }) {
 return (
     <main className='splash-main-container'>
         <section className='splash-section-1'>
-
+            <NavLink className='section-1-link' to={`/records/buy`}>
+                <h2 className='section-1-title'>
+                   SHOP NOW
+                   <i className="fa-solid fa-play section-1-play"></i>
+                </h2>
+            </NavLink>
         </section>
         <GetAllListings user={user} />
     </main>
