936f050cfec59993fb0044ddabb88e59cd05cbf6
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-16 16:42:44 -0500

    made section headings responsive

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -7,11 +7,19 @@ import GetAllListings from '../Listings/GetListings/GetAllListings';
 function SplashPage({ user }) {
 return (
     <main className='splash-main-container'>
-        <section className='splash-section-1'>
-            <NavLink className='section-1-link' to={`/records/buy`}>
-                <h2 className='section-1-title'>
+        <section className='splash-section-1 section'>
+            <NavLink className='section-1-link section-link' to={`/records/buy`}>
+                <h2 className='section-1-title section-title'>
                    SHOP NOW
-                   <i className="fa-solid fa-play section-1-play"></i>
+                   <i className="fa-solid fa-play section-1-play section-icon"></i>
+                </h2>
+            </NavLink>
+        </section>
+        <section className='splash-section-2 section'>
+            <NavLink className='section-2-link section-link' to={`/records/buy`}>
+                <h2 className='section-2-title section-title'>
+                   SELL
+                   <i className="fa-solid fa-play section-2-play section-icon"></i>
                 </h2>
             </NavLink>
         </section>
