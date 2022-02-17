d09abab678597aae0f8271fc51c6e6c31d0208bc
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-17 17:44:40 -0500

    showing details data

--- react-app/src/components/Listings/ListingDetails/ListingDetails.js
+++ react-app/src/components/Listings/ListingDetails/ListingDetails.js
@@ -1,9 +1,11 @@
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
+import placeholder from '../../../images/vinyl.jpg'
 import { getAllListings } from '../../../store/listings'
 import './ListingDetails.css'
-import { useEffect } from 'react'
+
 
 function ListingDetails({ user }) {
     const dispatch = useDispatch()
@@ -17,9 +19,23 @@ function ListingDetails({ user }) {
     const listing = listings[listingId]
 
     return (
-        <>
+        <main className='details-main-container'>
+            <section className='details-img-container'>
+                <img className='details-img' src={placeholder} />
+            </section>
         <h1>hello {listing?.id}</h1>
-        </>
+        <div className='sold-by-container'>
+            <p><strong>Sold by:</strong> {listing?.seller?.username}</p>
+            <img className='sold-by-img' src={listing?.seller?.profile_img_url ? listing?.seller?.profile_img_url : placeholder} />
+            <p><strong>Posted on:</strong> {listing?.created_at && new Date(listing?.created_at).toDateString()}</p>
+        </div>
+        <p><strong>Album:</strong> {listing?.album}</p>
+        <p><strong>Artist:</strong> {listing?.artist}</p>
+        <p><strong>Genre:</strong> {listing?.genre}</p>
+        <p><strong>Condition:</strong> {listing?.condition}</p>
+        <p><strong>Price:</strong> {listing?.price}</p>
+        <p><strong>Available copies:</strong> {listing?.num_copies_available}</p>
+        </main>
     )
 }
 
