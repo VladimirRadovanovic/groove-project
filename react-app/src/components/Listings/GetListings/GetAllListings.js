b24939fad3084177079573aa247bdcccba16861a
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-16 23:50:39 -0500

    refactoe classic listings and all listings to use one component

--- react-app/src/components/Listings/GetListings/GetAllListings.js
+++ react-app/src/components/Listings/GetListings/GetAllListings.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { getAllListings } from '../../../store/listings';
 import placeholder from '../../../images/vinyl.jpg'
@@ -30,20 +31,41 @@ function GetAllListings({ user }) {
         <h2 className='section-3-title'>RECORDS</h2>
         <div className='splash-article-container'>
                 {listingsList?.map(listing => (
-                    <article className='section-3-article' key={listing?.id}>
-                        <div className='section-3-img-container'>
-                            <img src={listing?.images?.img_url ? listing?.images?.img_url : placeholder}
-                             className='section-3-img' alt='record' />
+                    <article className='section-3-article article' key={listing?.id}>
+                        <div className='article-side article-front'>
+                            <div className='section-3-img-container article-img-container'>
+                                <img src={listing?.images?.img_url ? listing?.images?.img_url : placeholder}
+                                    className='section-3-img article-img' alt='record' />
+                            </div>
+                            <div className='article-front-text-container'>
+                            <p className='album-text'>
+                                {listing?.album}
+                            </p>
+                            <p className='artist-text'>
+                                {listing?.artist}
+                            </p>
+                            <p className='price-text'>
+                                ${listing?.price.toFixed(2)}
+                            </p>
+                            </div>
+
                         </div>
-                        {listing?.id}
-                        {listing?.album}
-                        {listing?.artist}
-                        {listing?.genre}
-                        {listing?.description}
+                        <div className='article-side article-back'>
+                            <div className='article-back-text-container'>
+                                <h3 className='article-back-heading'>Description</h3>
+                                <p className='article-back-text'>
+                                    {listing?.description}
+                                </p>
+                            </div>
+                            <div className='article-button-box'>
+                                <button id={listing?.id} className='cart-button'>Add to Cart</button>
+                                <NavLink to={`/records/${listing?.id}/details`} className='details-link'>View Details</NavLink>
+                            </div>
+                        </div>
                     </article>
                 ))}
 
-        </div>
+            </div>
         </>
     )
 }
