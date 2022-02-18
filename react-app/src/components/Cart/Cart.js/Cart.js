eb22bd22de54dd4ab907ae3a4b9170ff6c88cff4
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-18 09:58:55 -0500

    old state giving a warning

--- react-app/src/components/Cart/Cart.js/Cart.js
+++ react-app/src/components/Cart/Cart.js/Cart.js
@@ -1,14 +1,30 @@
-
+import { useState } from 'react'
 
 import './Cart.css'
+import placeholder from '../../../images/vinyl.jpg'
 
 function Cart({ user }) {
+    const [numItems, setNumItems] = useState(1)
+    const [itemId, setItemId] = useState('')
+    const [oldState, setOldState] = useState(1)
+
+
     const cartItems = Object.values(localStorage)
     console.log(cartItems.length, 'length')
     console.log(JSON.parse(cartItems[0]), 'cart items')
     const parsedItems = cartItems.map(item => (
         JSON.parse(item)
     ))
+
+    const handleChange = (e, prevState) => {
+        setNumItems(e.target.value)
+        setOldState(prevState)
+        const eventId = e.target.id.split('-')[1]
+        const inputField =document.getElementById(`item-${eventId}`)
+        console.log(inputField, 'eventId**********')
+        // inputField.value = numItems
+        setItemId(Number(eventId))
+    }
 
     return (
         <main className='main-cart-container'>
@@ -18,16 +34,39 @@ function Cart({ user }) {
                         Your Cart
                     </h2>
                     <button className='checkout-button-cart'>
-                    <i className="fa-solid fa-cart-shopping"></i>
+                        <i className="fa-solid fa-cart-shopping"></i>
                         Checkout
                     </button>
                 </div>
-        {parsedItems.map((item) => (
+                {parsedItems.map((item) => (
 
-            <div className='cart-article-container' key={item?.id}>
-                {item?.album}
-            </div>
-        ))}
+                    <div className='cart-article-container' key={item?.id}>
+                         <img className='cart-img' src={placeholder} />
+                        <div className='cart-data-container'>
+                            <p>
+                                {item?.album}
+                            </p>
+                            <p>
+                                {item?.artist}
+                            </p>
+                            <p>
+                                ${item?.price.toFixed(2)}
+                            </p>
+                        </div>
+                        <div className='quantity-container'>
+                             <input
+                             className='cart-input-field'
+                             id={`item-${item?.id}`}
+                             type='number'
+                            //  defaultValue={oldState}
+                             value={item?.id === itemId ? numItems : oldState}
+                             min={1}
+                             placeholder='Quantity'
+                             onChange={handleChange}
+                             />
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     )
