a04241bfbe51ba257c38bc519e4ef46475b0f3b9
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-21 19:20:50 -0500

    update modal styling

--- react-app/src/components/Orders/UpdateOrderForm.js
+++ react-app/src/components/Orders/UpdateOrderForm.js
@@ -4,12 +4,12 @@ import { useDispatch } from "react-redux";
 import { Modal } from "../../context/Modal";
 import { updateOrder } from "../../store/orders";
 
-function UpdateOrderForm({ onClose, onOpen, instructions, id}) {
+function UpdateOrderForm({ onClose, onOpen, instructions, id }) {
     const dispatch = useDispatch()
     const [newInstructions, setNewInstructions] = useState(instructions)
     const [errors, setErrors] = useState([])
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         const data = await dispatch(updateOrder(id, newInstructions))
         if (data) {
@@ -23,16 +23,25 @@ function UpdateOrderForm({ onClose, onOpen, instructions, id}) {
 
     return (
         <Modal onClose={onClose} >
-        <div>
-            <form onSubmit={handleSubmit}>
-                <textarea
-                value={newInstructions}
-                onChange={(e) => setNewInstructions(e.target.value)}
-                placeholder="Update delivery instructions."
-                ></textarea>
-                <button>Update instructions</button>
-            </form>
-        </div>
+            <div className="update-instructions-container">
+                <h3 className="modal-heading">Update delivery instructions</h3>
+                <div className='listings-errors-container'>
+                    <ul className='all-errors-list'>
+                        {errors.map(error => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                </div>
+                <form className="update-instructions-form" onSubmit={handleSubmit}>
+                    <textarea
+                    className="modal-textarea"
+                        value={newInstructions}
+                        onChange={(e) => setNewInstructions(e.target.value)}
+                        placeholder="Update delivery instructions."
+                    ></textarea>
+                    <button className="update-instructions-button">Update instructions</button>
+                </form>
+            </div>
         </Modal>
     )
 }
