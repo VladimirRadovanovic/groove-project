68101877ab818ffff866f2055d2e160612dbb52e
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-03-10 19:17:25 -0500

    error handling style fix

--- react-app/src/components/Listings/CreateListing/CreateListing.js
+++ react-app/src/components/Listings/CreateListing/CreateListing.js
@@ -156,7 +156,7 @@ function CreateListing({ user }) {
 
             <div className='listings-form-main-container'>
                 <div className='listings-errors-container'>
-                    <ul className='all-errors-list'>
+                    <ul className={errors?.length > 0 ? ('all-errors-list modal-errors-list') : ('hide-errors')}>
                         {errors.map(error => (
                             <li key={error}>{error}</li>
                         ))}
