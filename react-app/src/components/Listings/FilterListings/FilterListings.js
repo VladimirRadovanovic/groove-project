25ad531c1697c91063c5e6a883e04eeda43b3f47
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-03-11 09:34:24 -0500

    pre showcase

--- react-app/src/components/Listings/FilterListings/FilterListings.js
+++ react-app/src/components/Listings/FilterListings/FilterListings.js
@@ -8,6 +8,9 @@ function FilterListings({ filterByPrice }) {
     const [range, setRange] = useState(1000)
     const[drag,setDrag] = useState(1000)
 
+    let sliderRange = (range / 10 - 3).toString()
+    console.log('slider', sliderRange)
+
     console.log(range, 'rangeeeeee')
     return (
         <div className="filter-container">
@@ -15,6 +18,8 @@ function FilterListings({ filterByPrice }) {
             <p>Max price: ${Number(range)?.toFixed(2)}</p>
             <p className="price-1000">$1000.00</p>
             <p className="price-0">$0.00</p>
+        <div className="slider-container">
+            <div style={{bottom: `${sliderRange}%`}} className="slider-prev"><div className="slider-prev-text">${Number(range)?.toFixed(2)}</div></div>
         <input
         // style={{'accent-color': 'red'}}
         value={range}
@@ -25,6 +30,7 @@ function FilterListings({ filterByPrice }) {
         min='1'
         max='1000'
         />
+        </div>
         </div>
     )
 }
