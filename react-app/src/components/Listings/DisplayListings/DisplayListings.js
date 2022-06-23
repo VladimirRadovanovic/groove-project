90d8ce9f4935df0318f005939dee687c46f81b95
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-06-22 22:06:58 -0400

    news cards

--- react-app/src/components/Listings/DisplayListings/DisplayListings.js
+++ react-app/src/components/Listings/DisplayListings/DisplayListings.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react"
 
 import placeholder from '../../../images/vinyl.jpg'
 import FilterListings from "../FilterListings/FilterListings"
+import './DisplayListings.css'
 
 
 function DisplayListings({ listingsList, numItemSetter, searchedList }) {
@@ -17,7 +18,7 @@ function DisplayListings({ listingsList, numItemSetter, searchedList }) {
 
     useEffect(() => {
 
-        const fetchNews = async() => {
+        const fetchNews = async () => {
             const response = await fetch('/api/news')
             if (response.ok) {
                 const news = await response.json()
@@ -54,8 +55,8 @@ function DisplayListings({ listingsList, numItemSetter, searchedList }) {
         ))
         localStorage.setItem(id, JSON.stringify(cartListing[0]))
         let numItems = ''
-        if(localStorage.getItem('searched')) {
-             numItems = Object.values(localStorage).length - 1
+        if (localStorage.getItem('searched')) {
+            numItems = Object.values(localStorage).length - 1
         } else {
             numItems = Object.values(localStorage).length
         }
@@ -67,57 +68,62 @@ function DisplayListings({ listingsList, numItemSetter, searchedList }) {
     }
     return (
         <>
-        {news.map(n => (
-        <Link target="_blank" to={{pathname: 'https://www.kerrang.com' + n?.url }}>
+         <div className="news-container">
+            {news?.map((n, i) => (
+                <div className="news-card" key={i}>
+                    <Link className="news-card-link" target="_blank" to={{ pathname: 'https://www.kerrang.com' + n?.url }}>
+                        <img alt={n?.title.split('=')[1].split('"')[1]} src={`${n?.title.split('=')[3].slice(1)}`} />
+                        <span>
+                        {n?.title.split('=')[1].split('"')[1]}
 
-            <img alt={n?.title.split('=')[1].split('"')[1]} src={`${n?.title.split('=')[3].slice(1)}`} />
-            {n?.title.split('=')[1].split('"')[1]}
-        </Link>
-
-        ))}
+                        </span>
+                    </Link>
+                </div>
+            ))}
+            </div>
             {!url?.endsWith('/') && <FilterListings filterByPrice={filterByPrice} />}
-        <div className='splash-article-container'>
-            {filterListingsList?.map(listing => (
-                <article className='section-3-article article' key={listing?.id}>
-                    <div className='article-side article-front'>
-                        <div className='section-3-img-container article-img-container'>
-                            <img src={listing?.img_url ? listing?.img_url : placeholder}
-                                className='section-3-img article-img' alt='record' />
-                        </div>
-                        <div className='article-front-text-container'>
-                            <p className='album-text'>
-                                {listing?.album}
-                            </p>
-                            <p className='artist-text'>
-                                {listing?.artist}
-                            </p>
-                            <p className='price-text'>
-                                ${listing?.price.toFixed(2)}
-                            </p>
-                        </div>
+            <div className='splash-article-container'>
+                {filterListingsList?.map(listing => (
+                    <article className='section-3-article article' key={listing?.id}>
+                        <div className='article-side article-front'>
+                            <div className='section-3-img-container article-img-container'>
+                                <img src={listing?.img_url ? listing?.img_url : placeholder}
+                                    className='section-3-img article-img' alt='record' />
+                            </div>
+                            <div className='article-front-text-container'>
+                                <p className='album-text'>
+                                    {listing?.album}
+                                </p>
+                                <p className='artist-text'>
+                                    {listing?.artist}
+                                </p>
+                                <p className='price-text'>
+                                    ${listing?.price.toFixed(2)}
+                                </p>
+                            </div>
 
-                    </div>
-                    <div className='article-side article-back'>
-                        <div className='article-back-text-container'>
-                            <h3 className='article-back-heading'>Description</h3>
-                            <p className='article-back-text'>
-                                {listing?.description}
-                            </p>
                         </div>
-                        <div className='article-button-box'>
+                        <div className='article-side article-back'>
+                            <div className='article-back-text-container'>
+                                <h3 className='article-back-heading'>Description</h3>
+                                <p className='article-back-text'>
+                                    {listing?.description}
+                                </p>
+                            </div>
+                            <div className='article-button-box'>
 
                                 <button onClick={handleAddToCart} id={listing?.id} className='cart-button'>Add to Cart</button>
 
 
-                            {/* <AddToCart listing={listing} /> */}
-                            <NavLink to={`/records/${listing?.id}/details`} className='details-link'>View Details</NavLink>
+                                {/* <AddToCart listing={listing} /> */}
+                                <NavLink to={`/records/${listing?.id}/details`} className='details-link'>View Details</NavLink>
+                            </div>
                         </div>
-                    </div>
-                </article>
-            ))}
+                    </article>
+                ))}
 
-        </div>
-    </>
+            </div>
+        </>
     )
 }
 
